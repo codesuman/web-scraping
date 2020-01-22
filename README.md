52dd465bb73618d22077271e681df2faee499c1a
@codesuman <being.suman@yahoo.in>
2020-01-22 10:35:21 +0530

    Getting sub-categories

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 ## Image Scraping with Python :
 
 https://towardsdatascience.com/image-scraping-with-python-a96feda8af2d
+
+
+## Image Scraping :
+
+How to download image using requests
+
+https://stackoverflow.com/questions/13137817/a/18043472
--- scrapers/bigbasket/categories_scraper.py
+++ scrapers/bigbasket/categories_scraper.py
@@ -1,12 +1,12 @@
 import json
 
 from utils.http_req_res import getData
-from utils.file_io import createFiles, writeLineData, getBBPath, getBBCategoriesFileName
+from utils.file_io import createFiles, writeLineData, getBBCategoriesFileName
 
 
 class BigBasketCategoriesScraper:
     CATEGORIES_URL = "https://www.bigbasket.com/auth/get_menu/?city_id=1"
-    CATEGORIES_FILE_HEADER = "CATEGORY ID,CATEGORY NAME,CATEGORY SLUG\n"
+    CATEGORIES_FILE_HEADER = "CATEGORY ID,CATEGORY NAME,CATEGORY SLUG,URL\n"
 
     def __init__(self):
         createFiles(getBBCategoriesFileName(), self.CATEGORIES_FILE_HEADER)
@@ -29,8 +29,20 @@ class BigBasketCategoriesScraper:
             categories[top_category["id"]] = {"slug": top_category["slug"], "name": top_category["name"]}
 
             # Writing data to Categories file
-            category_data = str(top_category["id"]) + "," + top_category["name"] + "," + top_category["slug"] + "\n"
+            category_data = str(top_category["id"]) + "," + top_category["name"].replace(",","") + "," + top_category["slug"] + "," + top_category["url"] + "\n"
             writeLineData(getBBCategoriesFileName(), category_data)
 
-        return categories
+            # Fetching Sub-Categories
+            sub_cats = cat["sub_cats"][0]
+            for sub_cat in sub_cats:
+                sub_category = sub_cat["sub_category"]
+                category_data = str(top_category["id"]) + "," + sub_category[0].replace(",","") + "," + sub_category[1] + "," + sub_category[2] + "\n"
+                writeLineData(getBBCategoriesFileName(), category_data)
 
+                for cat in sub_cat["cats"]:
+                    catie = cat["cat"]
+                    category_data = str(top_category["id"]) + "," + catie[0].replace(",","") + "," + catie[1] + "," + \
+                                    catie[2] + "\n"
+                    writeLineData(getBBCategoriesFileName(), category_data)
+
+        return categories
